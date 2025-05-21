4b8fb4f8665b263a3a4683feee4070e3ea0402c8
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2025-05-21 19:44:28 +0530

    Add files via upload

---
+++ JavaProgram/Arithmetic.java
@@ -0,0 +1,31 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 5;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // challenge
+
+        int x = 4;
+        int y = 6;
+
+        System.out.println("\n" + x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+        // produvt of two floating point number
+
+        float no1 = 5.2f;
+        float no2 = 3.1f;
+
+        System.out.println("\n" + no1 * no2);
+
+    }
+}
