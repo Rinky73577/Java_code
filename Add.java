5a58184818287c8e9a367089aeff07c1e0ef54ce
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2026-03-24 23:25:00 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add{
+    static void add(int a, int b){
+        int sum = a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[]args){
+        add(10,5);
+        add(23,40);
+    }
+}
