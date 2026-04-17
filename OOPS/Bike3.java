ff26d1bd3400eba0cf1119555db245b302a53426
Rinky Kumari <113056646+Rinky73577@users.noreply.github.com>
2026-04-17 19:38:44 +0530

    Add files via upload

---
+++ OOPS/Bike3.java
@@ -0,0 +1,9 @@
+public class Bike3{
+    String name;
+    double price;
+
+    Bike3(String name, double price){
+        this.name=name;
+        this.price=price;
+    }
+}
---
+++ OOPS/Bike3Details.java
@@ -0,0 +1,9 @@
+public class Bike3Details{
+    public static void main(String[] args) {
+        Bike3 b1 = new Bike3("R1",34);
+        System.out.println(b1.name + " " + b1.price);
+
+        Bike3 b2 = new Bike3("honda",76);
+        System.out.println(b2.name + " " + b2.price);
+    }
+}
